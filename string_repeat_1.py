bbeb953b4162346d8ac97d7af61dd95a51459690
shayyal-py <65650601+shayyal-py@users.noreply.github.com>
2020-05-20 13:45:54 +0530

    Created string_repeat_1.py

---
+++ string_repeat_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+
+# Strings can be repeated using *
+
+var = 'Hi' * 3
+
+print ("String after Hi * 3 : ", var)
+
+var1 = 3 * 'Hi'
+
+print ("String after 3 * Hi : ", var1)
+
+print ("String after repetation 3 * 'un' + 'mm' : ", 3 * 'un' + 'mm')
+print ("String after repetation 'un' + 3 * 'mm' : ", 'un' + 3 * 'mm')
+print ("String after repetation 'un' + 'mm' * 3 : ", 'un' + 'mm' * 3)
