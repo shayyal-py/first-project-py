70664334e4fdb9f6c6e3b84dbc20a6abbada3947
shayyal-py <65650601+shayyal-py@users.noreply.github.com>
2020-05-20 13:43:42 +0530

    Created string_print_1.py

---
+++ string_print_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.5
+
+# Single quotes
+print ('Hello World !');
+
+# Use \' to escape the single quote in single quote
+print ('It doesn\'t contain')
+
+# Else use double quote to supress
+print ("It doesn't contain")
+
+# Use \" to escape the double quote
+print ("\"Yes\", We are")
+
+# Else use single quote supress it
+print ('"Yes", we are')
+
+# With print(), \n produces a new line
+string = 'First line \nSecond line'
+print(string)
