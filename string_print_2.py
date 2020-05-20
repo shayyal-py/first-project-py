cb0309e691f8d52dbb31b0caa820c09f78b42ac3
shayyal-py <65650601+shayyal-py@users.noreply.github.com>
2020-05-20 13:44:51 +0530

    Created string_print_2.py

---
+++ string_print_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.5
+
+# If you don’t want characters prefaced by \ to be interpreted as special characters,
+# you can use raw strings by adding an r before the first quote
+
+# In below line \n means newline!
+print('C:\some\name')
+
+# Note the r before the quote to supress \n meaning i.e special charecter meaning
+print(r'C:\some\name')
